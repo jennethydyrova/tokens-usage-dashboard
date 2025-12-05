f4ba88b6c5b4c7cc4b428d904bf28a2b9c73f316
jennethydyrova <jennethydyrovaa@gmail.com>
2025-12-05 16:05:12 +0000

    move padLength to var

--- frontend/src/lib/formatters/date.ts
+++ frontend/src/lib/formatters/date.ts
@@ -2,14 +2,16 @@
 // work with dates extensively, I would use dayjs or similar to make life simpler
 // and ensure higher accuracy.
 export const formatISO = (isoString: string): string => {
+  // Number of digits to pad for day, month, hours, minutes
+  const padLength = 2;
   const date = new Date(isoString);
 
-  const day = String(date.getUTCDate()).padStart(2, "0");
-  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(date.getUTCDate()).padStart(padLength, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(padLength, "0");
   const year = date.getUTCFullYear();
 
-  const hours = String(date.getUTCHours()).padStart(2, "0");
-  const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+  const hours = String(date.getUTCHours()).padStart(padLength, "0");
+  const minutes = String(date.getUTCMinutes()).padStart(padLength, "0");
 
   return `${day}-${month}-${year} ${hours}:${minutes}`;
 };
